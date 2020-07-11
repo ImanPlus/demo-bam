401218ea77743a6fdba462ad161167c0a2d8e12a
iman <i.jalali88@gmail.com>
2020-07-11 15:10:21 +0430

    insert Resource component in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './css/App.css';
 
+import Resource from './Component/Resource'
+
 function App() {
   return (
     <div>
-
+      <Resource/>
     </div>
   );
 }
