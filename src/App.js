a5fc877f93ec832dd2a8b067ff347a615c24ef25
iman <i.jalali88@gmail.com>
2020-07-12 10:22:46 +0430

    change resource component to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import './css/App.css';
+import * as React from "react";
+import {Admin, Resource, ListGuesser} from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
-import Resource from './Component/Resource'
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-function App() {
-  return (
-    <div>
-      <Resource/>
-    </div>
-  );
-}
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={ListGuesser}/>
+  </Admin>
+);
 
 export default App;
