3737d5629a175b2c0814f55b354ab5387d21c6e7
iman <i.jalali88@gmail.com>
2020-07-13 20:41:49 +0430

    Assign MyLayout to dashboard layout feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import {Admin, Resource, ListGuesser} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import MyLayout from "./Layout/MyLayout";
+
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} layout={MyLayout}>
     <Resource name="users" list={ListGuesser}/>
   </Admin>
 );
