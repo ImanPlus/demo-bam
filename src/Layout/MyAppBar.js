51d7e73dce102941d651ac218d620dbb67d0d40f
iman <i.jalali88@gmail.com>
2020-07-14 13:26:34 +0430

    create MyAppBar component for Layout section

---
+++ src/Layout/MyAppBar.js
@@ -0,0 +1,59 @@
+import React from "react";
+import clsx from 'clsx';
+import AppBar from '@material-ui/core/AppBar';
+
+import {makeStyles, useTheme} from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+
+//----------------- Styles -----------------
+const drawerWidth = 240;
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+}))
+
+//----------------- Main Function -----------------
+const MyAppBar = ({props, valueOpen, drawerOpen}) => {
+  const theme = useTheme();
+  const classes = useStyles();
+
+  return (
+    <AppBar
+      position="fixed"
+      className={clsx(classes.appBar, {
+        [classes.appBarShift]: valueOpen,
+      })}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={drawerOpen()}
+          edge="start"
+          className={clsx(classes.menuButton, valueOpen && classes.hide)}
+        >
+          <MenuIcon/>
+        </IconButton>
+        <Typography variant="h6" noWrap>
+          Persistent drawer
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
+export default MyAppBar;
