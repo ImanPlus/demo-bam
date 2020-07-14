e3acb87f96c50329aed4c0bd9e03b43719f3b8ac
iman <i.jalali88@gmail.com>
2020-07-14 13:27:33 +0430

    fixed MyLayout and adding Appbar componnet

--- src/Layout/MyLayout.js
+++ src/Layout/MyLayout.js
@@ -1,16 +1,40 @@
 import React from "react";
-import { Layout } from 'react-admin';
-// import MyAppBar from './MyAppBar';
+import {Layout} from 'react-admin';
+import MyAppBar from './MyAppBar';
 import MySidebar from './MySidebar';
 // import MyMenu from './MyMenu';
 // import MyNotification from './MyNotification';
 
-const MyLayout = props => <Layout
-  {...props}
-  // appBar={MyAppBar}
-  sidebar={MySidebar}
-  // menu={MyMenu}
-  // notification={MyNotification}
-/>;
+// const MyLayout = props => <Layout
+//   {...props}
+//    appBar={MyAppBar}
+//   sidebar={MySidebar}
+//    menu={MyMenu}
+//    notification={MyNotification}
+// />;
+import {makeStyles} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+}))
+
+function MyLayout(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline/>
+      <MyAppBar valueOpen={open} drawerOpen={handleDrawerOpen}/>
+    </div>
+  )
+}
 
 export default MyLayout;
