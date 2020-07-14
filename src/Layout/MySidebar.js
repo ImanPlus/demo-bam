887c7570a541de4fb33ce87437a6439c075f496f
iman <i.jalali88@gmail.com>
2020-07-14 23:19:28 +0430

    create MySidebar component

--- src/Layout/MySidebar.js
+++ src/Layout/MySidebar.js
@@ -1,25 +1,72 @@
 import React from "react";
 import {Sidebar} from 'react-admin';
-import {makeStyles} from '@material-ui/core/styles';
 
-const useSidebarStyles = makeStyles({
+import Drawer from '@material-ui/core/Drawer';
+import {makeStyles, useTheme} from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Divider from '@material-ui/core/Divider';
+
+const drawerWidth = 240;
+const useSidebarStyles = makeStyles((theme) => ({
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
   drawerPaper: {
-    backgroundColor: 'red',
+    width: drawerWidth,
   },
-  textcontex: {
-    color: 'red',
-    backgroundColor: 'blue',
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
   },
-});
+  appSidebarLogoIcon: {
+    borderRadius: '.29rem',
+    width: '40px',
+    height: '40px',
+    background: '#fff',
+    display: 'flex',
+    alignItems: 'center',
+    alignContent: 'center',
+    boxShadow: '1px 1px 3px rgba(7,9,25,.2)',
+  },
+}));
 
-const MySidebar = props => {
+const MySidebar = ({props, valueOpen, drawerClose}) => {
   const classes = useSidebarStyles();
+  const theme = useTheme();
+  console.log('open',valueOpen)
   return (
-    <Sidebar style={{backgroundColor: 'red'}} classes={classes.textcontex} {...props}>
+    <Drawer
+      className={classes.drawer}
+      variant="persistent"
+      anchor="left"
+      open={valueOpen}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <div className={classes.drawerHeader}>
+        <IconButton onClick={drawerClose()}>
+          {theme.direction === 'ltr' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
+        </IconButton>
+      </div>
+      <div className={classes.appSidebarLogoIcon}>
+        <img alt="Bamburgh React Crypto Application with Material-UI PRO"
+             src="logo.svg"
+        />
+        ااعاعهاعاه
+      </div>
+      <Divider/>
 
-      <h3>This is Test</h3>
-      <div className={classes.textcontex}>iman jalali</div>
-    </Sidebar>
+      <Divider/>
+
+    </Drawer>
   );
 };
 
