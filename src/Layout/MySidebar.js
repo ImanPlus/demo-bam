a2612189992b7ef8eafb5512380297059e527689
iman <i.jalali88@gmail.com>
2020-07-15 13:35:10 +0430

    create header sidebar

--- src/Layout/MySidebar.js
+++ src/Layout/MySidebar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Sidebar} from 'react-admin';
+import Logo from './logo.svg'
 
 import Drawer from '@material-ui/core/Drawer';
 import {makeStyles, useTheme} from '@material-ui/core/styles';
@@ -8,7 +8,8 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Divider from '@material-ui/core/Divider';
 
-const drawerWidth = 240;
+//----------------- Styles -----------------
+const drawerWidth = 340;
 const useSidebarStyles = makeStyles((theme) => ({
   drawer: {
     width: drawerWidth,
@@ -23,24 +24,51 @@ const useSidebarStyles = makeStyles((theme) => ({
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
+    justifyContent: 'space-between',
   },
   appSidebarLogoIcon: {
-    borderRadius: '.29rem',
-    width: '40px',
-    height: '40px',
-    background: '#fff',
+    background: '#3c44b1',
+    boxShadow: '1px 1px 3px rgba(7,9,25,.2)',
+    //   '& img':{
+    // width: '26px',
+    // margin: '0 auto',
+    // -webkitAanimation: 'logo-spin 20s linear infinite',
+    // animation: 'logo-spin 20s linear infinite',
+    //   },
+  },
+  app_sidebar_logo_text: {
+    lineHeight: 1,
+    padding: '0 0 0 .66667rem',
+    transition: 'all .5s cubic-bezier(.685,.0473,.346,1)',
+    display: 'flex',
+    flexDirection: 'column',
+    '& b': {
+      fontSize: '1.265rem',
+      fontWeight: 700,
+    },
+    '& span': {
+      display: 'block',
+      textTransform: 'uppercase',
+      fontSize: '.72917rem',
+      padding: '4px 0',
+    },
+  },
+  app_sidebar_logo: {
     display: 'flex',
     alignItems: 'center',
-    alignContent: 'center',
-    boxShadow: '1px 1px 3px rgba(7,9,25,.2)',
-  },
+    transition: 'all .5s cubic-bezier(.685,.0473,.346,1)',
+    '& a': {
+      display: 'contents',
+      textDecoration: 'none',
+    },
+  }
 }));
 
+//----------------- Main Function -----------------
 const MySidebar = ({props, valueOpen, drawerClose}) => {
   const classes = useSidebarStyles();
   const theme = useTheme();
-  console.log('open',valueOpen)
+  console.log('open', valueOpen)
   return (
     <Drawer
       className={classes.drawer}
@@ -52,16 +80,24 @@ const MySidebar = ({props, valueOpen, drawerClose}) => {
       }}
     >
       <div className={classes.drawerHeader}>
-        <IconButton onClick={drawerClose()}>
+        <div className={classes.app_sidebar_logo}>
+          <a href="/">
+            <div className={classes.appSidebarLogoIcon}>
+              <img
+                src={Logo}
+                alt="App-logo" width="50" height="50"/>
+            </div>
+            <div className={classes.app_sidebar_logo_text}>
+              <span>Crypto</span>
+              <b>bamburgh</b>
+            </div>
+          </a>
+        </div>
+        <IconButton onClick={() => drawerClose()} style={{backgroundColor: '#c9ede7'}}>
           {theme.direction === 'ltr' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
         </IconButton>
       </div>
-      <div className={classes.appSidebarLogoIcon}>
-        <img alt="Bamburgh React Crypto Application with Material-UI PRO"
-             src="logo.svg"
-        />
-        ااعاعهاعاه
-      </div>
+
       <Divider/>
 
       <Divider/>
@@ -71,3 +107,6 @@ const MySidebar = ({props, valueOpen, drawerClose}) => {
 };
 
 export default MySidebar
+
+
+
