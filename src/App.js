b404859c5f061c0e805fddeee795cffe963b4004
iman <i.jalali88@gmail.com>
2020-07-18 10:38:21 +0430

    init icon Resource section name

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import * as React from "react";
 import {Admin, Resource, ListGuesser} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import MyLayout from "./Layout/MyLayout";
-
+import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider} layout={MyLayout}>
-    <Resource name="users" list={ListGuesser}/>
+    <Resource name="users" list={ListGuesser} icon={UserIcon}/>
   </Admin>
 );
 
